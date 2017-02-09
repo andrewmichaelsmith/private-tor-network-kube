de542293386a44a7eb53c7ff6b2fcacbfa3f89bc
Andrew Smith <me@andrewmichaelsmith.com>
2017-02-09 20:56:12 +0000

    Setup kubecfg properly and stop hardcoding server location

--- setup.sh
+++ setup.sh
@@ -11,21 +11,21 @@ set -x
 ### 6. Using certs (1), keys (2) and Service IP (5) add to da_list
 ### 7. Add da_list to torrc for EACH da
 ### 8. Create ConfigMaps for certs (1), keys (2), torrc (7)
-### 9. Create Deployment
+### 9. Create da1, da2, da3 Deployment
+### 10. Using da_list create torrc for relays
+### 11. Create relay (1 deployment, scaled to multiple relays)
 
 
 make_da () {
 	name=$1
-
-	echo "making name $name"
 	mkdir -p $name
 	cd $name
 
 	#Need service now because we need IP address for da_list AND Address conig
 	name=$name envsubst < ../da-svc-template.yml > da-svc.yml
-	kubectl -s http://192.168.122.254:8080 delete -f da-svc.yml
-	kubectl -s http://192.168.122.254:8080 create -f da-svc.yml
-	ip=$( kubectl -s http://192.168.122.254:8080 get -f da-svc.yml -o 'jsonpath={.spec.clusterIP}')
+	kubectl delete -f da-svc.yml
+	kubectl create -f da-svc.yml
+	ip=$( kubectl get -f da-svc.yml -o 'jsonpath={.spec.clusterIP}')
 
 
 	#For --list-fignerpinrt
@@ -52,11 +52,11 @@ make_relay() {
 	cp ../torrc-base torrc
 	cat ../da_list >> torrc
 
-	kubectl -s http://192.168.122.254:8080 delete secret generic relay-torrc
-	kubectl -s http://192.168.122.254:8080 create secret generic relay-torrc --from-file torrc
+	kubectl delete secret generic relay-torrc
+	kubectl create secret generic relay-torrc --from-file torrc
 
-	kubectl -s http://192.168.122.254:8080 delete -f ../relay.yml
-	kubectl -s http://192.168.122.254:8080 create -f ../relay.yml
+	kubectl delete -f ../relay.yml
+	kubectl create -f ../relay.yml
 
 	cd ..
 }
@@ -64,8 +64,8 @@ make_relay() {
 	
 
 ##Hack until we bake it in
-kubectl -s http://192.168.122.254:8080 delete configmap tor-entry
-kubectl -s http://192.168.122.254:8080 create configmap tor-entry --from-file docker-entrypoint.sh
+kubectl delete configmap tor-entry
+kubectl create configmap tor-entry --from-file docker-entrypoint.sh
 
 rm da_list
 
@@ -81,15 +81,15 @@ cat da_list >> da3/torrc
 for name in da1 da2 da3
 do
 	#secrets cuz configmaps don't do binary
-	kubectl -s http://192.168.122.254:8080 delete secret $name-id
-	kubectl -s http://192.168.122.254:8080 delete secret $name-keys
-	kubectl -s http://192.168.122.254:8080 delete secret $name-torrc
-	kubectl -s http://192.168.122.254:8080 delete -f $name/da.yml
+	kubectl delete secret $name-id
+	kubectl delete secret $name-keys
+	kubectl delete secret $name-torrc
+	kubectl delete -f $name/da.yml
 
-	kubectl -s http://192.168.122.254:8080 create secret generic $name-id --from-file $name #TODO: this picks up torrc+other shit
-	kubectl -s http://192.168.122.254:8080 create secret generic $name-keys --from-file $name/keys
-	kubectl -s http://192.168.122.254:8080 create secret generic $name-torrc --from-file $name/torrc
-	kubectl -s http://192.168.122.254:8080 create -f $name/da.yml
+	kubectl create secret generic $name-id --from-file $name #TODO: this picks up torrc+other shit
+	kubectl create secret generic $name-keys --from-file $name/keys
+	kubectl create secret generic $name-torrc --from-file $name/torrc
+	kubectl create -f $name/da.yml
 done
 
 
