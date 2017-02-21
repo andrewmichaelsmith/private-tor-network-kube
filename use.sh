2a83020e76925e42d110014eb8c824a9080cb1ab
Andrew Smith <me@andrewmichaelsmith.com>
2017-02-21 21:30:19 +0000

    use a random relay each time

--- use.sh
+++ use.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-relay=$(kubectl get pods --selector=app=tor-relay -o name | head -n 1)
+relay=$(kubectl get pods --selector=app=tor-relay -o name | sort -R | head -n 1)
 relay=${relay:4}
 kubectl port-forward $relay 9050:9050
 
